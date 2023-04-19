5ce1a641b5e4b598cd3db5247cab9e6905e9f4b8
Christine Dickens <christine.dickens@utexas.edu>
2023-04-18 22:41:25 -0500

    Posts page outline

---
+++ src/Posts.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Posts = () => {
+
+    return (
+        <React.Fragment>
+            <h1>Cars For Sale</h1>
+        </React.Fragment>
+    )
+}
+
+export default Posts;
