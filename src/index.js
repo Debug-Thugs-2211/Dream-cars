5f8ac401dab1065502e3e765e2d1c52e5b07a611
Abed <abedj2023@Gmail.com>
2023-04-19 10:27:49 -0500

    added some packages

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import Posts from './Posts';
+import React from "react";
+import Posts from "./Posts";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
-    return (
-        <h1>Dream Cars</h1>
-    )
-}
+  return (
+    <div>
+      <h1>Dream Cars</h1>
+      <button className="btn btn-primary">Hello</button>
+    </div>
+  );
+};
 
-import { createRoot } from 'react-dom/client';
-const container = document.getElementById('root');
-const root = createRoot(container); 
+import { createRoot } from "react-dom/client";
+const container = document.getElementById("root");
+const root = createRoot(container);
 root.render(<App />);
