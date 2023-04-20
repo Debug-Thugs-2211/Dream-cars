7d6fb6292a132b85b4335b7b27bed8d45580be51
Christine Dickens <christine.dickens@utexas.edu>
2023-04-20 10:15:05 -0500

    DB tables and seed data

--- db/cars.js
+++ db/cars.js
@@ -1,6 +1,8 @@
+const client = require('./client');
+
 const createCar = async ({ make, model, year, color, mileage }) => {
     try {
-        const { rows: [cars ]} = await client.query(
+        const { rows: [cars]} = await client.query(
         `
         INSERT INTO cars(make, model, year, color, mileage)
         VALUES ($1, $2, $3, $4, $5)
