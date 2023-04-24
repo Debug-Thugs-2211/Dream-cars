565483f72fb71b0a9d3902c027a2d12d72d555d2
Abed <abedj2023@Gmail.com>
2023-04-24 10:49:48 -0500

    created home page

--- api/index.js
+++ api/index.js
@@ -1,5 +1,33 @@
 const express = require("express");
 const router = express.Router();
+const { JWT_SECRET } = process.env;
+const jwt = require("jsonwebtoken");
+
+router.use(async (req, res, next) => {
+  const prefix = "Bearer ";
+  const auth = req.header("authorization");
+  if (!auth) {
+    next();
+  } else if (auth.startsWith(prefix)) {
+    const token = auth.slice(prefix.length);
+
+    try {
+      const user = jwt.verify(token, JWT_SECRET);
+
+      if (user.id) {
+        req.user = await getUserById(user.id);
+        next();
+      }
+    } catch ({ name, message }) {
+      next({ name, message });
+    }
+  } else {
+    next({
+      name: "AuthorizationHeaderError",
+      message: `Authorization token must start with ${prefix}`,
+    });
+  }
+});
 
 router.get("/health", async (req, res, next) => {
   try {
--- src/index.js
+++ src/index.js
@@ -3,18 +3,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import NavBar from "./components/NavBar";
 import Posts from "./components/Posts";
 import "./App.css";
 import { Login } from "./components/Login";
 import Register from "./components/Register";
 import Checkout from "./components/checkout";
+import Home from "./components/Home";
 
 const App = () => {
   return (
     <div id="app">
       <NavBar />
       <Routes>
+        <Route path="/" exact element={<Home />} />
         <Route path="/Posts" element={<Posts />} />
         <Route path="Login" element={<Login />} />
         <Route path="Register" element={<Register />} />
