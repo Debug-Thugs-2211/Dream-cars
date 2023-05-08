b4d5289a32de2c2786db64778ddb49658e9675b4
Gitonga <richierichman15@gmail.com>
2023-05-08 10:47:41 -0500

    almost done with home page, having troblems with css. Couldn't get the divs where I want them to go.

---
+++ src/Components/Footer.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <>
+         <div>
+        <h1>Website Developers</h1>
+
+        <img href src=""></img>
+        <h2>Abed Jamous</h2>
+        <a href='https://github.com/AbedJ2023'>GitHub</a>
+
+        <img href src=""></img>
+        <h2>Christine Dickens</h2>
+        <a href="https://github.com/cdickens14">GitHub</a>
+
+        <img href src=""></img>
+        <h2>Emily Cooke</h2>
+        <a href="https://github.com/EmSabineCooke">GitHub</a>
+
+        <img href src=""></img>
+        <h2>Gitonga Nyaga</h2>
+        <a href="https://github.com/Richierichman15">GitHub</a>
+      </div>
+        </>
+    )
+};
+
+export default Footer;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,35 +1,45 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Home.css";
 
 const Home = () => {
+  let slides = document.getElementsByClassName("mySlides");
 
-  // const displaySlides = (nums) => {
+useEffect(() => {
+  showSlides(1)
+}, [slides.length])
 
-  //   const currentIndex = 1;
-  //   displaySlides(currentIndex);
-  //   console.log('df',currentIndex);
+const showSlides = (n) => {
+  let i;
+  if (!slides.length) {
+    return;
+  }
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  
+  slides[slideIndex-1].style.display = "block";
+  console.log('slides', slides);
+}
+let slideIndex = 1;
+showSlides(slideIndex);
 
-  //   let i;
+const plusSlides = (n) => {
+  console.log('here', n);
+  showSlides(slideIndex += n);
 
-  //   const slides = document.getElementsByClassName('imageSlides');
-  //   if (nums > slides.length) { currentIndex }
-  //   if (nums < 1) {
-  //      currentIndex = slides.length;
-  //   }
-  //   for ( i = 0; i < slides.length; x++) {
-  //     slides[x].style.display = "none";
-  //   }
-  //   slides[currentIndex -  1].style.display = 'block';
-  // }
-  // const setSlides = (num) => {
-  //   displaySlides(currentIndex += num);
-  // }
+}
+
+const currentSlide = (n) => {
+  showSlides(slideIndex = n);
+}
 
   return (
-    <>
+    <div id="background">
       <div className="cont-1">
         <h1 id="title">Find your Dream Car!</h1>
-        <p>
+        <p id="welcome">
            Welcome to our webpage, where you can shop for cars to fulfill your transportation needs. 
            We are thrilled to have you here and are committed to providing you an exeptional shopping experience.
            Where you're looking for a stylish sedan, a practical SUV, or a powerful sports car, we have a wide range of options to suit your preferences and budget.
@@ -37,76 +47,57 @@ const Home = () => {
         </p>
       <ul>
         <li>
-          <p>Low Cost!</p>
-          <p>Amazing Customer Service!</p>
-          <p>Money-back Guarantee</p>
-          <p>Multiple Brands</p>
+          <p className="checkMark">Low Cost!</p>
+          <p className="checkMark">Amazing Customer Service!</p>
+          <p className="checkMark">Money-back Guarantee</p>
+          <p className="checkMark">Multiple Brands</p>
         </li>
       </ul>
       </div>
-    <body>
-    <div className="images-slidshow">
-      <div className="imageSlides fade">
-           <img href src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8JMjwzimnXSO22nl7fqmXvq5XFBX_89PXEw&usqp=CAU"></img>
+
+      <div className="slideshow-container">
+       <div className="mySlides fade">
+          <div className="numbertext">1 / 6</div>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8JMjwzimnXSO22nl7fqmXvq5XFBX_89PXEw&usqp=CAU" ></img>
+       </div>
+
+      <div className="mySlides fade">
+        <div className="numbertext">2 / 6</div>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQE1H2AVAb9oX8yf23DawGfQv5oDS6utBO7QA&usqp=CAU" ></img>
       </div>
 
-      <div className="imageSlides fade">
-           <img href src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQE1H2AVAb9oX8yf23DawGfQv5oDS6utBO7QA&usqp=CAU"></img>
+      <div className="mySlides fade">
+        <div className="numbertext">3 / 6</div>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyCFzY_3i_4Pe9ixuIRXcKTkzHqFibqipENg&usqp=CAU" ></img>
       </div>
 
-      <div className="imageSlides fade">
-           <img href src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyCFzY_3i_4Pe9ixuIRXcKTkzHqFibqipENg&usqp=CAU"></img>
-        </div>
+      <div className="mySlides fade">
+        <div className="numbertext">4 / 6</div>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJBZcvwo7Qf7Ldb0E5YyRYmFTdhX2aYdDSuA&usqp=CAU" ></img>
+      </div>
 
-        <div className="imageSlides fade">
-          <img href src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbAjD8pEom0w1T3MPSxjr_QvzY13_NWP2uBw&usqp=CAU"></img>
-        </div>
+      <div className="mySlides fade">
+        <div className="numbertext">5 / 6</div>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6rxyJkJ3c9vdRwdjA7VQlWYI22NvLx1FslQ&usqp=CAU" ></img>
+      </div>
 
-        <div className="imageSlides fade">
-          <img href src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXNQiGgqFIjAyidDUUr76qdbhosjHsN6QUvg&usqp=CAU"></img>
-        </div>
+      <div className="mySlides fade">
+        <div className="numbertext">6 / 6</div>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQo0DxGvGoAey1UWx88eq9fXKUlP37u1FFKUA&usqp=CAU" ></img>
+      </div>
 
-        <div className="imageSlides fade">
-          <img href src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcyv8pm4JGBKurSARgoaR6jmbsSR0pxB83kA&usqp=CAU"></img>
-        </div>
-
-        <div className="imageSlides fade">
-          <img href src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTncFou1O5mb0xKQcpXVlkATY9vgSbjnTA3qg&usqp=CAU"></img>
-        </div>
-        {/* <a class="slider-btn previous" onClick={setSlides(-1)}></a>
-        <a class="slider-btn next" onClick={setSlides(1)}></a> */}
-    </div>
-    </body>
-    <body>
+  <a className="prev" onClick={() => plusSlides(-1)}>&#10094;</a>
+  <a className="next" onClick={() => plusSlides(1)}>&#10095;</a>
+</div>
       <div>
-        <p>
+        <p className="thankyou">
           Thank you for taking the time to visit our website. 
           We appreciate your interest in our selection of cars and hope that our platform provides you with a seamless shopping experience. 
           If you have any questions or feedback, please don't hesitate to reach out to our friendly and knowledgeable team.
           We look forward to helping you find your perfect car!
         </p>
       </div>
-      <div>
-        <h1>Website Developers</h1>
-
-        <img href src=""></img>
-        <h2>Abed Jamous</h2>
-        <a href='https://github.com/AbedJ2023'>GitHub</a>
-
-        <img href src=""></img>
-        <h2>Christine Dickens</h2>
-        <a href="https://github.com/cdickens14">GitHub</a>
-
-        <img href src=""></img>
-        <h2>Emily Cooke</h2>
-        <a href="https://github.com/EmSabineCooke">GitHub</a>
-
-        <img href src=""></img>
-        <h2>Gitonga Nyaga</h2>
-        <a href="https://github.com/Richierichman15">GitHub</a>
-      </div>
-    </body>
-    </>
+    </div>
   )
 };
 
